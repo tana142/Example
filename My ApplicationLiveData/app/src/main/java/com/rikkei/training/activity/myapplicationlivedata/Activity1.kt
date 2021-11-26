c65f317e0e01af3a995c6e271ad5ae9f3ef2fefb
TanBD <tanbd@rikkeisoft.com>
2021-11-26 14:57:46 +0700

    tan add activiti1

---
+++ My ApplicationLiveData/app/src/main/java/com/rikkei/training/activity/myapplicationlivedata/Activity1.kt
@@ -0,0 +1,11 @@
+package com.rikkei.training.activity.myapplicationlivedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_1)
+    }
+}
