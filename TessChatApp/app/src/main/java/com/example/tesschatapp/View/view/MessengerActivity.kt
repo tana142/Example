870aa1f2c44194e9734170d46cdf8bbc85fe32c0
DucNM <ducnmph13062@fpt.edu.vn>
2021-11-30 17:37:50 +0700

    login

--- TessChatApp/app/src/main/java/com/example/tesschatapp/View/view/MessengerActivity.kt
+++ TessChatApp/app/src/main/java/com/example/tesschatapp/View/view/MessengerActivity.kt
@@ -1,9 +1,9 @@
-package com.example.tesschatapp
+package com.example.tesschatapp.View.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 import android.view.WindowManager
+import com.example.tesschatapp.R
 
 class MessengerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
