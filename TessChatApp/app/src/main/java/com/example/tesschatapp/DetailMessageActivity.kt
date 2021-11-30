fb3e8f6d91d191c633d95859113c60dfe05e5ed5
TanBD <tanbd@rikkeisoft.com>
2021-11-30 17:39:50 +0700

    change_detail_message

--- TessChatApp/app/src/main/java/com/example/tesschatapp/DetailMessageActivity.kt
+++ TessChatApp/app/src/main/java/com/example/tesschatapp/DetailMessageActivity.kt
@@ -1,11 +1,14 @@
 package com.example.tesschatapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class DetailMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_message)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE or WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
     }
 }
